9db686089af4d6bf1f8c30117d98f50b12fa9d2e
varchasvi <varchasvee108@gmail.com>
2026-04-14 19:11:46 +0530

    transformer layer

--- model/layers.py
+++ model/layers.py
@@ -22,3 +22,41 @@ class SinusoidalEmbeddings(nn.Module):
 
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.config = config
+        self.ln1 = nn.LayerNorm(config.model.n_embd)
+        self.ln2 = nn.LayerNorm(config.model.n_embd)
+        self.attn = nn.MultiheadAttention(
+            embed_dim=config.model.n_embd,
+            num_heads=config.model.num_heads,
+            dropout=config.model.dropout,
+            batch_first=True,
+        )
+        self.mlp = nn.Sequential(
+            nn.Linear(config.model.n_embd, config.model.hidden_dim),
+            nn.GELU(),
+            nn.Linear(config.model.hidden_dim, config.model.n_embd),
+        )
+
+        self.adaln_mod = nn.Sequential(
+            nn.SiLU(), nn.Linear(config.model.n_embd, 6 * config.model.n_embd)
+        )
+
+    def forward(self, x: torch.Tensor, t_emb: torch.Tensor) -> torch.Tensor:
+
+        chunks = self.adaln_mod(t_emb).chunk(6, dim=-1)
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = chunks
+
+        x_norm = self.ln1(x)
+        x_mod = x_norm * (1 + scale_msa) + shift_msa
+
+        attn, _ = self.attn(x_mod, x_mod, x_mod)
+        x = x + gate_msa * attn
+
+        x_mod = self.ln2(x) * (1 + scale_mlp) + shift_mlp
+        x = x + gate_mlp * self.mlp(x_mod)
+        return x
