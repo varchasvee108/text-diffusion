7e4c5f43f0b7ff654a39186d417655d8ce3a9fc1
varchasvi <varchasvee108@gmail.com>
2026-04-15 14:06:13 +0530

    model.py updated with adaln layer

--- model/model.py
+++ model/model.py
@@ -8,6 +8,7 @@ class DiffusionModel(nn.Module):
     def __init__(self, config: Config):
         super().__init__()
         self.config = config
+
         if config.data.vocab_size is None:
             raise ValueError(
                 "vocab isze must be defined and set in config before initialisation"
@@ -20,7 +21,7 @@ class DiffusionModel(nn.Module):
             SinusoidalEmbeddings(config.model.time_embd),
             nn.Linear(config.model.time_embd, 4 * config.model.time_embd),
             nn.SiLU(),
-            nn.Linear(4 * config.model.time_embd, config.model.time_embd),
+            nn.Linear(4 * config.model.time_embd, config.model.n_embd),
         )
         self.transformer_blocks = nn.ModuleList(
             [TransformerBlock(config=config) for _ in range(config.model.num_layers)]
@@ -32,4 +33,18 @@ class DiffusionModel(nn.Module):
         self.proj_out = nn.Linear(config.model.n_embd, config.model.n_embd)
 
     def forward(self, x, t):
-        B, T, C = x.shape
+        B, T = x.shape
+
+        x = self.tok_embd(x) + self.pos_embd[:, :T, :]
+
+        t_emb = self.time_embd(t)  # (B, n_embd)
+
+        for block in self.transformer_blocks:
+            x = block(x, t_emb)
+
+        shift, scale = self.adaln_mod(t_emb).unsqueeze(1).chunk(2, dim=-1)
+
+        x = self.ln_f(x)
+        x = x * (1 + scale) + shift
+        x = self.proj_out(x)
+        return x
