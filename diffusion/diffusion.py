4809059153ed12fb8a53d23b899e1298cd30ba8b
varchasvi <varchasvee108@gmail.com>
2026-04-16 13:58:22 +0530

    diffusion code

--- diffusion/diffusion.py
+++ diffusion/diffusion.py
@@ -1,5 +1,6 @@
 import torch
 from core.config import Config
+from tqdm import tqdm
 
 
 class Diffusion:
@@ -22,3 +23,50 @@ class Diffusion:
         self.sqrt_alpha_cumprod = torch.sqrt(self.alpha_cumprod)
         self.sqrt_one_minus_alpha_cumprod = torch.sqrt(1.0 - self.alpha_cumprod)
         self.sqrt_recip_alpha = torch.sqrt(1.0 / self.alphas)
+
+    def sample_timesteps(self, batch_size: int):
+        return torch.randint(
+            self.timesteps, (batch_size,), device=self.device, dtype=torch.long
+        )
+
+    def add_noise_to_embeddings(self, x0, t, noise=None) -> torch.Tensor:
+
+        if noise is None:
+            noise = torch.randn_like(x0)
+
+        sqrt_alpha_hat = self.sqrt_alpha_cumprod[t].view(-1, 1, 1)
+        sqrt_one_minus = self.sqrt_one_minus_alpha_cumprod[t].view(-1, 1, 1)
+
+        return sqrt_alpha_hat * x0 + sqrt_one_minus * noise
+
+    def predict_x0(self, x_t, t, noise_pred):
+        sqrt_alpha_hat = self.sqrt_alpha_cumprod[t].view(-1, 1, 1)
+        sqrt_one_minus = self.sqrt_one_minus_alpha_cumprod[t].view(-1, 1, 1)
+
+        return (x_t - sqrt_one_minus * noise_pred) / sqrt_alpha_hat
+
+    def p_sample(self, model, x, t, temp=1.0):
+        beta = self.betas[t].view(-1, 1, 1)
+        alpha = self.alphas[t].view(-1, 1, 1)
+        alpha_hat = self.alpha_cumprod[t].view(-1, 1, 1)
+
+        noise_pred = model(x, t)
+
+        mean = self.sqrt_recip_alpha[t].view(-1, 1, 1) * (
+            x - ((1 - alpha) / torch.sqrt(1 - alpha_hat)) * noise_pred
+        )
+        noise = torch.randn_like(x)
+        mask = (t > 0).float().view(-1, 1, 1)
+        return mean + (mask * torch.sqrt(beta * temp) * noise)
+
+    @torch.inference_mode()
+    def sample(self, model, shape, temp=1.0):
+        model.eval()
+        x = torch.randn(shape, device=self.device)
+        pbar = tqdm(reversed(range(self.config.diffusion.timesteps)))
+
+        for t in pbar:
+            t_batch = torch.full((shape[0],), t, device=self.device, dtype=torch.long)
+            x = self.p_sample(model, x, t_batch, temp=temp)
+
+        return x
